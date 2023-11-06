ec7b61280e7faf0e42c1b6c614fe98ef00d2600c
Raylson L. Moreira <raylson.lima@outlook.com>
2023-11-06 10:56:34 -0300

    implementa o componente table

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,5 +1,71 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { ReducerType } from '../types';
+import { actionWalletSucessRemove } from '../redux/actions';
+
 function Table() {
-  return <div>Table</div>;
+  const dispatch = useDispatch();
+  const { expenses } = useSelector((state: ReducerType) => state.wallet);
+  const handleBtnDelete = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    event.preventDefault();
+    dispatch(actionWalletSucessRemove({ data: parseFloat(event.currentTarget.id) }));
+  };
+
+  console.log(useSelector((state: ReducerType) => state.wallet.expenses));
+  return (
+
+    <table>
+      <thead>
+        <tr>
+          <th>Descrição</th>
+          <th>Tag</th>
+          <th>Método de pagamento</th>
+          <th>Valor</th>
+          <th>Moeda</th>
+          <th>Câmbio utilizado</th>
+          <th>Valor convertido</th>
+          <th>Moeda de conversão</th>
+          <th>Editar/Excluir</th>
+        </tr>
+      </thead>
+      {
+        expenses.map(({ id,
+          description,
+          tag, method,
+          value,
+          currency,
+          exchangeRates,
+        }) => (
+          <tbody key={ id }>
+            <tr>
+              <td>{description}</td>
+              <td>{ tag }</td>
+              <td>{method}</td>
+              <td>{`${parseFloat(value).toFixed(2)} ${currency}`}</td>
+              <td>{exchangeRates[currency].name}</td>
+              <td>{`${parseFloat(exchangeRates[currency].ask).toFixed(2)} BRL`}</td>
+              <td>
+                {`${(parseFloat(exchangeRates[currency].ask)
+                * parseFloat(value)).toFixed(2)} BRL`}
+              </td>
+              <td>{exchangeRates[currency].name.split('/')[1]}</td>
+              <td>
+                <button>Editar</button>
+                <button
+                  id={ id.toString() }
+                  data-testid="delete-btn"
+                  onClick={ (event) => { handleBtnDelete(event); } }
+                >
+                  Excluir
+
+                </button>
+              </td>
+            </tr>
+          </tbody>
+        ))
+      }
+
+    </table>
+  );
 }
 
 export default Table;
--- src/redux/actions/index.ts
+++ src/redux/actions/index.ts
@@ -1,4 +1,10 @@
+import { Dispatch } from '../../types';
+
 export const USER = 'USER';
+export const WALLET_START = 'WALLET_START';
+export const WALLET_SUCESS_ADD = 'WALLET_SUCESS_ADD';
+export const WALLET_FAILED = 'WALLET_FAILED';
+export const WALLET_SUCESS_REMOVE = 'WALLET_SUCESS_REMOVE';
 
 const userDefault = {
   email: '',
@@ -8,3 +14,37 @@ export const actionUser = (data = userDefault) => ({
   type: USER,
   payload: data,
 });
+
+export const actionWalletStart = () => ({
+  type: WALLET_START,
+});
+
+export const actionWalletSucessAdd = (data: string[] | object) => ({
+  type: WALLET_SUCESS_ADD,
+  payload: data,
+});
+
+export const actionWalletSucessRemove = (data: { data: number }) => ({
+  type: WALLET_SUCESS_REMOVE,
+  payload: data,
+});
+
+export const actionWalletFailed = (data: any) => ({
+  type: WALLET_FAILED,
+  payload: data,
+});
+
+export const fetchWallet = (type?: string, form?: object) => {
+  return async (dispatch: Dispatch) => {
+    try {
+      dispatch(actionWalletStart());
+      const response = await fetch('https://economia.awesomeapi.com.br/json/all');
+      const data = await response.json();
+      delete data.USDT;
+      if (type === 'array') dispatch(actionWalletSucessAdd(Object.keys(data)));
+      else dispatch(actionWalletSucessAdd({ ...form, exchangeRates: data }));
+    } catch (error: any) {
+      dispatch(actionWalletFailed(error.message));
+    }
+  };
+};
